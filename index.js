e8873a578052d416ea453033de573eac5c738370
Richard Crespo <rjcrespo96@gmail.com>
2021-03-27 16:13:27 -0500

    Mensaje listo

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3000;
 
 app.listen(port, host, () => {
-  console.log('El servidor funcionando.');
+  console.log('El servidor está LISTO!');
 })
